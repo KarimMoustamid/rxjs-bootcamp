b691aedc4b4e7cde4bba5627e582b497530a93dd
Karim Moustamid <karimmoustamid@MacBook-Air-Midnight.local>
2024-09-07 18:57:43 -0600

    into to using predefined rxjs observebles

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,40 +1,43 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {
-    concat,
-    fromEvent,
-    interval,
-    noop,
-    observable,
-    Observable,
-    of,
-    timer,
-    merge,
-    Subject,
-    BehaviorSubject,
-    AsyncSubject,
-    ReplaySubject
-} from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
-
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { fromEvent, interval, timer } from "rxjs";
 
 @Component({
-    selector: 'about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
+  ngOnInit() {
+    //     document.addEventListener("click", (evt) => {
+    //       console.log("click event :", evt);
+    //       setTimeout(() => {
+    //         let i = 0;
+    //         setInterval(() => {
+    //           console.log("interval:", i);
+    //         }, 1000);
+    //       }, 3000);
+    //     });
 
-    ngOnInit() {
+    const interval$ = timer(3000, 1000);
+    const subInterval1 = interval$.subscribe((v) =>
+      console.log("stream 1 => " + v)
+    );
+    const subInterval2 = interval$.subscribe((v) =>
+      console.log("stream 2 => " + v)
+    );
 
+    setTimeout(() => {
+      subInterval1.unsubscribe();
+      subInterval2.unsubscribe();
+    }, 5000);
 
-    }
+    const click$ = fromEvent(document, "click");
+    const subClick = click$.subscribe(
+      (c) => console.log("click$ : ", c),
+      (e) => console.log(e),
+      () => console.log("completed")
+    );
 
-
+    subClick.unsubscribe();
+  }
 }
-
-
-
-
-
-
